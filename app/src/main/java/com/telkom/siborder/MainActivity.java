501985ab1e7323593f23e008abd61ade857b8047
AndikaBayuS <andika.bayu.s2001@gmail.com>
2018-01-12 17:32:53 +0700

    Initial Commit

---
+++ app/src/main/java/com/telkom/siborder/MainActivity.java
@@ -0,0 +1,40 @@
+package com.telkom.siborder;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private SectionPagerAdapter mSectionpageAdapter;
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSectionpageAdapter = new SectionPagerAdapter(getSupportFragmentManager());
+
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+        tabLayout.getTabAt(0).setText("Node B");
+        tabLayout.getTabAt(1).setText("Maps");
+        tabLayout.getTabAt(2).setText("Quadric");
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new Tab1Fragment());
+        adapter.addFragment(new Tab2Fragment());
+        adapter.addFragment(new Tab3Fragment());
+        viewPager.setAdapter(adapter);
+    }
+}
