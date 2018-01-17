2c93a567d2997909bc462b8ff652c7bb58017422
F4uzan <f4uzan@this.is.f4>
2018-01-17 08:59:00 +0000

    Remove MapActivity and rename activity_map

--- app/src/main/java/com/telkom/siborder/MapFragment.java
+++ app/src/main/java/com/telkom/siborder/MapFragment.java
@@ -12,11 +12,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
@@ -39,7 +37,7 @@ public class Tab2Fragment extends Fragment implements OnMapReadyCallback {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        return inflater.inflate(R.layout.activity_map, container,false);
+        return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
     @Override
