5ad79787dc17e5bf4332aa0e08348e73ccfb1609
F4uzan <f4uzan@this.is.f4>
2018-01-23 13:29:54 +0000

    Make splash screen less annoying and obnoxious
    We were actively blocking the UI with sleep in Thread; this causes the app to practically hang in splash until the sleep is accomplished.
    
    While this was okay at first, if the user were to, say, switch to another app during the splash, startActivity will still be called (as the timer is still running) and it’ll be spawned right wherever the user is. Hence, interrupting them (and surprising them, at times).
    
    Fix this by using Runnable which allows for the app to be interrupted, but the transition to LoginActivity won’t be interrupted at all and the activity won’t be spawned if the user exits or pauses the app.
    
    * Also add a constant delay for the animation and the runnable itself. We need them both in sync.
    * Reduce the animation duration to 1000 miliseconds

--- app/src/main/java/com/telkom/siborder/SplashActivity.java
+++ app/src/main/java/com/telkom/siborder/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.telkom.siborder;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -8,29 +10,28 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        ImageView iv = findViewById(R.id.iv);
 
+        Integer delay = 1000;
+        ImageView iv = findViewById(R.id.iv);
         Animation myanim = AnimationUtils.loadAnimation(this, R.anim.transition);
+
+        myanim.setDuration(delay);
         iv.startAnimation(myanim);
+
         final Intent login = new Intent(this, com.telkom.siborder.LoginActivity.class);
 
-        Thread timer = new Thread(){
-            public void run (){
-                try {
-                    sleep(5000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                finally {
-                    startActivity(login);
-                    finish();
-                }
+        Handler handler = new Handler(Looper.getMainLooper());
+        final Runnable runnable = new Runnable() {
+            public void run() {
+                startActivity(login);
+                finish();
             }
         };
-        timer.start();
+        handler.postDelayed(runnable, delay);
     }
 }
