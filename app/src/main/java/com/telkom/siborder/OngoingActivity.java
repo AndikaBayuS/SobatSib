a9b48ee22f179a30210a68ed86d72dac029afa77
Andika Bayu Santoso <andika.bayu.s2001@gmail.com>
2018-02-27 11:25:15 +0700

    Revert "Ongoing : Add RecyclerView"
    This reverts commit f47ce1140031ce6194e8b1a794e3179d365befc2.

--- app/src/main/java/com/telkom/siborder/OngoingActivity.java
+++ app/src/main/java/com/telkom/siborder/OngoingActivity.java
@@ -8,8 +8,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,21 +16,15 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 
-public class OngoingActivity extends AppCompatActivity {
+public class OngoingActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     private static final String TAG = "MainActivity";
 
-    ListView listView;
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
     private TextView mDisplayDate1;
     private TextView mDisplayDate2;
     private Spinner spinner1;
@@ -47,17 +39,101 @@ public class OngoingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ongoing);
 
-        ArrayList<ListItem> listItem = new ArrayList<>();
-        listItem.add(new ListItem("Line 1","Line 4"));
-        listItem.add(new ListItem("Line 3","Line 4"));
+        Button next = findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(OngoingActivity.this, UpActivity.class);
+                startActivity(next);
+                overridePendingTransition(0, 0);
+            }
+        });
+        mDisplayDate1 = (TextView) findViewById(R.id.tvDialog);
+        mDisplayDate1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int mounth = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
 
-        mRecyclerView = findViewById(R.id.recyclerView);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(this);
-        mAdapter = new ListAdapter(listItem);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        OngoingActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDataSetListener1,year,mounth,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+        mDisplayDate2 = (TextView) findViewById(R.id.tvDate);
 
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        mDisplayDate2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int mounth = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        OngoingActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDataSetListener2,year,mounth,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDataSetListener1 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG,"onDataSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate1.setText(date);
+
+            }
+        };
+        mDataSetListener2 = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                Log.d(TAG,"onDataSet: mm/dd/yyy: " + month + "/" + day + "/" + year);
+
+                String date = month + "/" + day + "/" + year;
+                mDisplayDate2.setText(date);
+
+            }
+        };
+        spinner1 = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.batch_array, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(this);
+
+        spinner2 = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.priority_array, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+        spinner3= findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.project_array, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(this);
+
+        spinner4= findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,
+                R.array.btspos_array, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+        spinner4.setOnItemSelectedListener(this);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavView_Bar);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
@@ -91,5 +167,15 @@ public class OngoingActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/telkom/siborder/SectionPagerAdapter.java
+++ app/src/main/java/com/telkom/siborder/SectionPagerAdapter.java
@@ -7,22 +7,22 @@ import android.support.v4.app.FragmentPagerAdapter;
 import java.util.ArrayList;
 import java.util.List;
 
-    public class SectionPagerAdapter extends FragmentPagerAdapter {
+public class SectionPagerAdapter extends FragmentPagerAdapter {
 
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        SectionPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    SectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
 
-        @Override
-        public Fragment getItem(int position) {
-            return mFragmentList.get(position);
-        }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
 
-        @Override
-        public int getCount() {
-            return mFragmentList.size();
-        }
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
     void addFragment(Fragment fragment){
         mFragmentList.add(fragment);
     }
